96ecbe0c10a3d8b85e9908b1bd3ed7069c8b3f81
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-24 11:55:51 -0400

    dynamically adjust the appearance of a view based on its size and location

---
+++ SwiftUI Examples/ViewAppearance.swift
@@ -0,0 +1,59 @@
+//
+//  ViewAppearance.swift
+//  SwiftUI Examples
+//
+//  Created by Parth Antala on 2024-07-24.
+//
+
+import SwiftUI
+
+struct ViewAppearance: View {
+    @State private var rotationAmount = 0.0
+    var body: some View {
+        VStack {
+            ScrollView {
+                ForEach(0..<100) { i in
+                    Text("row \(i)")
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity)
+                        .visualEffect { content, proxy in
+                            content.blur(radius: blurAmount(for: proxy))
+                        }
+                }
+            }
+            .padding(20)
+            
+            Grid {
+                ForEach(0..<3) { i in
+                    GridRow {
+                        ForEach(0..<3) { j in
+                            Circle()
+                                .fill(.orange)
+                                .frame(width: 50, height: 50)
+                                .visualEffect { content, proxy in
+                                    content.hueRotation(.degrees(proxy.frame(in: .global).midY / 2))
+                                }
+                        }
+                    }
+                }
+            }
+            .rotationEffect(.degrees(rotationAmount))
+            .onAppear {
+                withAnimation(.linear(duration: 5).repeatForever(autoreverses: false)) {
+                    rotationAmount = 360
+                }
+            }
+        }
+    }
+    
+    func blurAmount(for proxy: GeometryProxy) -> Double {
+        let scrollViewHeight = proxy.bounds(of: .scrollView)?.height ?? 100
+        let ourCenter = proxy.frame(in: .scrollView).midY
+        let distanceFromCenter = abs(scrollViewHeight / 2 - ourCenter)
+        return Double(distanceFromCenter / 100)
+    }
+}
+
+#Preview {
+    ViewAppearance()
+}
