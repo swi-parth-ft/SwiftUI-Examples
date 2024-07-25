1b1aa6d7e4350e070a8b106765951d238a13706b
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-25 11:04:32 -0400

    scroll to exact locations

--- SwiftUI Examples/Scroll.swift
+++ SwiftUI Examples/Scroll.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Scroll: View {
+    @State private var items = 100
+    @State private var position = ScrollPosition(edge: .top)
     var body: some View {
         VStack {
             ScrollView {
@@ -25,6 +27,43 @@ struct Scroll: View {
                         .padding(.horizontal)
                 }
             }
+            
+            ScrollView {
+                ForEach(0..<items, id: \.self) { i in
+                    Text("Row \(i)")
+                }
+            }
+            .onScrollGeometryChange(for: Double.self) { geo in
+                geo.contentSize.height
+            } action: { oldValue, newValue in
+                print("Height is now \(newValue)")
+            }
+            
+            Button("Add row") { items += 1 }
+            
+            ScrollView {
+                ForEach(0..<100) { i in
+                    Text("item \(i)")
+                        .padding()
+                        .id(i)
+                }
+            }
+            .scrollPosition($position)
+            
+            HStack(spacing: 50) {
+                Button("Top") {
+                    position.scrollTo(edge: .top)
+                }
+                
+                Button("Bottom") {
+                    position.scrollTo(edge: .bottom)
+                }
+                
+                Button("Random") {
+                    position.scrollTo(id: Int.random(in: 0..<100))
+                }
+            }
+            
         }
     }
 }
